c4e73605549278be4ce43f7b876ccc98887cb747
Aarush Tanwar <tanwaraarush007@gmail.com>
2021-06-19 22:37:32 +0530

    fixed a bug with localstorage of light and dark mode

--- script.js
+++ script.js
@@ -32,10 +32,11 @@ function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
-        localStorage.setItem('theme', 'light');
+        
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 }
